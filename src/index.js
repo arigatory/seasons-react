a0c0d368e4b2747523601baf1ef02b0f894fc418
Ivan MSI <arigatory@gmail.com>
2022-02-02 00:01:30 +0300

    Passing State as Props

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: ''};
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude});
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
-        );
-    }
+	render() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
 
-    render() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>;
+		if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
-        if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
-        }
+		return <div>Loading!</div>;
+	}
 
-        return <div>Loading!</div>;
-
-    }
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
